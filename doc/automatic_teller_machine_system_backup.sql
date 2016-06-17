9d6b7b00f040e2ed814ad642e8768b412a0bda1c
myyerrol <myyerrol@126.com>
2016-06-17 10:45:10 +0800

    Update the project's information

---
+++ doc/automatic_teller_machine_system_backup.sql
@@ -0,0 +1,79 @@
+-- MySQL dump 10.13  Distrib 5.5.46, for debian-linux-gnu (x86_64)
+--
+-- Host: localhost    Database: automatic_teller_machine_system
+-- ------------------------------------------------------
+-- Server version	5.5.46-0ubuntu0.14.04.2
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Table structure for table `card_information`
+--
+
+DROP TABLE IF EXISTS `card_information`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `card_information` (
+  `card_id` varchar(10) NOT NULL DEFAULT '',
+  `card_pwd` varchar(10) DEFAULT NULL,
+  `card_money` int(10) NOT NULL,
+  PRIMARY KEY (`card_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `card_information`
+--
+
+LOCK TABLES `card_information` WRITE;
+/*!40000 ALTER TABLE `card_information` DISABLE KEYS */;
+INSERT INTO `card_information` VALUES ('000000','100000',5000),('000001','100001',15000),('000002','100002',2500),('000003','100003',1500),('000004','100004',4500);
+/*!40000 ALTER TABLE `card_information` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `user_information`
+--
+
+DROP TABLE IF EXISTS `user_information`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `user_information` (
+  `user_name` varchar(15) NOT NULL,
+  `user_age` varchar(5) NOT NULL,
+  `user_sex` varchar(5) NOT NULL,
+  `user_phone` varchar(15) NOT NULL,
+  `card_id` varchar(10) DEFAULT NULL,
+  PRIMARY KEY (`user_name`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `user_information`
+--
+
+LOCK TABLES `user_information` WRITE;
+/*!40000 ALTER TABLE `user_information` DISABLE KEYS */;
+INSERT INTO `user_information` VALUES ('刘一','18','男','13234329374','000000'),('张三','15','男','19388423225','000002'),('李四','25','男','13849239862','000003'),('王五','35','男','18829389283','000004'),('陈二','20','女','18364593942','000001');
+/*!40000 ALTER TABLE `user_information` ENABLE KEYS */;
+UNLOCK TABLES;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2016-06-17 10:42:43
